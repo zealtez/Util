9318517282f975e97fb5f5aa22d544bcef936b85
zealtez <zealtez@gmail.com>
2018-03-21 16:06:22 +0900

    delete javascript/main.js and rename javascript folder
    to arrange the inside directory

--- javascript/main.js
+++
@@ -1,5 +0,0 @@
-#Get the yesterdayDate
-function getYesterdaysDate(date) {
-    var yesterday = new Date((new Date(date)).valueOf() - 1000*60*60*24);
-    return yesterday.getFullYear()  + '-' + (yesterday.getMonth()+1) + '-' + yesterday.getDate();
-}
