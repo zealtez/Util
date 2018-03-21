f55481d81f2b4575ac29afc21cf6a1a43fe537cc
zealtez <zealtez@naver.com>
2018-03-21 16:09:29 +0900

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,5 @@
-
+#Get the yesterdayDate
+function getYesterdaysDate(date) {
+    var yesterday = new Date((new Date(date)).valueOf() - 1000*60*60*24);
+    return yesterday.getFullYear()  + '-' + (yesterday.getMonth()+1) + '-' + yesterday.getDate();
+}
