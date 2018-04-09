f48a518ddde01f0b24f067051de60ff34f6dc9ce
zealtez <zealtez@naver.com>
2018-04-09 13:31:24 +0900

    Update String.js

--- Javascript/String.js
+++ Javascript/String.js
@@ -3,12 +3,12 @@
 ------------------------------------------------------
 
 var str = document.getElementById("textarea").value;
-str = str.replace(/(?:\r\n|\r|\n)/g, '<br/>');
-document.getElementById("textarea").value = str;
+    str = str.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+    document.getElementById("textarea").value = str;
 
 ------------------------------------------------------
 #Input textArea enter Handling while Extracting from DB 
 ------------------------------------------------------
 var str = document.getElementById("textarea").value;
-str = str.replaceAll("<br/>", "\r\n");
-document.getElementById("textarea").value = str;
+    str = str.replaceAll("<br/>", "\r\n");
+    document.getElementById("textarea").value = str;
