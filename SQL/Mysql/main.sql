bc09f2c30269cc668a6ec20ee2ec0e782ff7eb2a
zealtez <zealtez@naver.com>
2018-04-09 20:39:45 +0900

    Update main.sql

--- SQL/Mysql/main.sql
+++ SQL/Mysql/main.sql
@@ -4,4 +4,7 @@
 
 -- #inst_seq   varchar(8) NOT NULL
 SELECT 
-	CONCAT( 'A', LPAD( CONCAT(CAST( IFNULL(SUBSTR( MAX(inst_seq), 2), 0) AS UNSIGNED ) +1 ), '5', 0 ) ) AS NEXT_SEQ 
+	CONCAT( 'A', LPAD( CONCAT(CAST( IFNULL(SUBSTR( MAX(board_seq), 2), 0) AS UNSIGNED ) +1 ), '5', 0 ) ) AS NEXT_SEQ 
+-- #inst_seq   SMALLINT   NOT NULL
+SELECT 
+	IFNULL( MAX(board_seq), 0)  +1  AS NEXT_SEQ 
