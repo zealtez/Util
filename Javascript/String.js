acde7ee82fb041de1e8caf209283335da36a2cf6
zealtez <zealtez@naver.com>
2018-04-09 11:43:44 +0900

    Create String.js

---
+++ Javascript/String.js
@@ -0,0 +1,14 @@
+------------------------------------------------------
+#Input textArea enter Handling while Entering into DB 
+------------------------------------------------------
+
+var str = document.getElementById("textarea").value;
+str = str.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+document.getElementById("textarea").value = str;
+
+------------------------------------------------------
+#Input textArea enter Handling while Extracting from DB 
+------------------------------------------------------
+var str = document.getElementById("textarea").value;
+str = str.replaceAll("<br/>", "\r\n");
+document.getElementById("textarea").value = str;
