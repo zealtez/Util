aff57f3d80108d7ed6b63828528aaa32199b83d5
zealtez <zealtez@naver.com>
2018-04-12 03:19:12 +0900

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -20,3 +20,16 @@ function updateFileList() {
 	 output.innerHTML += '</p>';
 }
 
+-----------------------
+#Leading Zeros
+-----------------------
+function leadingZeros(n, digits) {
+  var zero = '';
+  n = n.toString();
+
+  if (n.length < digits) {
+    for (var i = 0; i < digits - n.length; i++)
+      zero += '0';
+  }
+  return zero + n;
+}
