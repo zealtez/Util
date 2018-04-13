0d896d85cfa47a91a37d1817be83b8018b0e7f86
zealtez <zealtez@naver.com>
2018-04-13 21:48:12 +0900

    Create plot.py

---
+++ Python/pandas/plot.py
@@ -0,0 +1,14 @@
+------------------------------------------------------
+#correlation matrix
+------------------------------------------------------
+def plot_corr(df,size=14):
+    '''Function plots a graphical correlation matrix for each pair of columns in the dataframe.
+
+    Input:
+        df: pandas DataFrame
+        size: vertical and horizontal size of the plot'''
+
+    corr = df.corr()
+    fig, ax = plt.subplots(figsize=(size, size))
+    ax.matshow(corr)
+    plt.xticks(range(len(corr.columns)), corr.columns);
