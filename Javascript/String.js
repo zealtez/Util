1fd377d26cd153e8c60afa5e8d9356a178e80e85
zealtez <zealtez@naver.com>
2018-06-29 01:35:00 +0900

    Update String.js

--- Javascript/String.js
+++ Javascript/String.js
@@ -12,3 +12,42 @@ var str = document.getElementById("textarea").value;
 var str = document.getElementById("textarea").value;
     str = str.replaceAll("<br/>", "\r\n");
     document.getElementById("textarea").value = str;
+
+------------------------------------------------------
+# PruneStrign OverFlow
+------------------------------------------------------
+    function fnPruneStringOverflow(width){
+	
+	var wrapperWidth = width;
+	var $ownerList = $($('#adOwnerList').find('label'));
+
+	$ownerList.each(function(idx,line){
+		var inputId = line.htmlFor;
+		var ownerText = line.innerText;
+		var ownerTextLength = $.fn.textWidth(ownerText);
+
+		if(ownerTextLength >= wrapperWidth){
+			var wordWidth = ownerTextLength / ownerText.length;
+			ownerText = ownerText.substring(0,Math.floor(wrapperWidth/wordWidth)) +'...';
+		}
+
+		console.log(ownerText);
+
+	});
+
+	$.fn.textWidth = function(text){
+		  var org = $(this)
+		  var html = $('<span style="postion:absolute;width:auto;left:-9999px">' + (text || org.html()) + '</span>');
+		  if (!text) {
+		    html.css("font-family", org.css("font-family"));
+		    html.css("font-size", org.css("font-size"));
+		  }
+		  $('body').append(html);
+		  var width = html.width();
+		  html.remove();
+		  return width;
+	}
+
+}
+
+------------------------------------------
