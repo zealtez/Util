d9928648d75ea7330de528118b7b8091f895b086
zealtez <zealtez@naver.com>
2018-06-29 18:31:10 +0900

    Create regex.js

---
+++ Javascript/regex.js
@@ -0,0 +1,5 @@
+/*----------------------
+* yyyymmdd -> yyyy-mm-dd
+------------------------*/
+minDate = minDate.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+maxDate = maxDate.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
